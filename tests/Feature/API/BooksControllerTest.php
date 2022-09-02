c7a9ed0355b4913a88c03827561dfde4c4110450
= <bethaniafreitas@yahoo.com.br>
2022-09-02 15:33:57 -0300

    aula02

--- tests/Feature/API/BooksControllerTest.php
+++ tests/Feature/API/BooksControllerTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\API;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Book;
 use Illuminate\Testing\Fluent\AssertableJson;
@@ -11,27 +10,32 @@ use Illuminate\Testing\Fluent\AssertableJson;
 
 class BooksControllerTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_books_get_endpoint()
+    public function test_get_books_endpoint()
     {
-        // $book = Book::factory()->create();
+        $books = Book::factory(3)->create();
         $response = $this->getJson('/api/books');
 
         $response->assertStatus(200);
-        $response->assertJsonCount(5);
+        $response->assertJsonCount(3);
 
-        $response->assertJson(function(AssertableJson $json){
-            $json->whereType('0.id', 'integer');
-            $json->whereType('0.title', 'string');
-            $json->whereType('0.isbn', 'string');
+
+        $response->assertJson(function (AssertableJson $json) use($books){
+            $json->whereAllType([
+                '0.id' => 'integer',
+                '0.title' => 'string',
+                '0.isbn' => 'string'
+            ]);
+
 
             $json->hasAll(['0.id', '0.title', '0.isbn']);
 
-            
+            $book = $books->first();
 
             $json->whereAll([
                 '0.id' => $book->id,
